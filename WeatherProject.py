7115e45d8adb764b6c538a75c0d452645241380f
HLStanley <hstanley.267@student.rcsnc.org>
2023-12-05 10:03:14 -0500

    commit

--- WeatherProject.py
+++ WeatherProject.py
@@ -1,9 +1,10 @@
 import requests
 import PySimpleGUI as sg
 
-API_KEY = 'b75ddaaeed0cac26e7405723281dc1ec' #My API Key
-sg.theme('DarkBlue1') #Theme for GUI
+API_KEY = 'b75ddaaeed0cac26e7405723281dc1ec' # My API Key
+sg.theme('DarkBlue1') # Sets the theme for the GUI
 
+# THIS PORTION OF CODE CREATES THE GUI
 layout = [
  [sg.Text('Current Weather')],
  [sg.Text('Input your city'), sg.InputText(key='-CITY-'), sg.Button('Enter')], #Asks user to input city
@@ -13,7 +14,7 @@ layout = [
 
 window = sg.Window('Weather App', layout)
 
-# Create a session object
+# Create a session object which provides persistance via cookies allowing the code to call the API faster
 session = requests.Session()
 
 headers = {'Accept-Encoding': 'gzip'}
@@ -23,6 +24,7 @@ while True:
  if event == sg.WIN_CLOSED: # Closes if user closes window
      break
  
+# THIS PORTION OF CODE CHECKS IF THE CITY EXISTS ADN THEN RETURNS THE DATA TO THE GUI
  if event == 'Enter':
      city = values['-CITY-'] #Asocciates Input to Get Request
      URL = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
